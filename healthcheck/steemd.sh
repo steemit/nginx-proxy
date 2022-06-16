33297565fffa4538bfbd8ecdfc60a6fe5f823de5
ety001 <work@akawa.ink>
2022-06-16 23:37:53 +0000

    fix healthcheck bash

--- healthcheck/steemd.sh
+++ healthcheck/steemd.sh
@@ -18,12 +18,14 @@ if [[ ${BLOCKCHAIN_TIME} == "null" ]]; then
   exit 0
 fi
 
+BLOCKCHAIN_TIME=`echo $BLOCKCHAIN_TIME | sed 's/T/\ /g'`
+
 if [[ ! -z  "$BLOCKCHAIN_TIME" ]]; then
   BLOCKCHAIN_SECS=`date -d $BLOCKCHAIN_TIME +%s`
   CURRENT_SECS=`date +%s`
 
   # if we're within 60 seconds of current time, call it synced and report healthy
-  BLOCK_AGE=$((${CURRENT_SECS} - ${BLOCKCHAIN_SECS}))
+  BLOCK_AGE=$(${CURRENT_SECS} - ${BLOCKCHAIN_SECS})
   if [[ ${BLOCK_AGE} -le 60 ]]; then
     echo Status: 200
     echo Content-type:text/plain
